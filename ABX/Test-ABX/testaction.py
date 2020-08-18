4c450d0be77f0e5dd6074e340ee222da10a75237
Andy-Knight <39732302+Andy-Knight@users.noreply.github.com>
2020-08-18 16:53:01 +0100

    Action Version: 4

--- ABX/Test-ABX/testaction.py
+++ ABX/Test-ABX/testaction.py
@@ -8,7 +8,7 @@ def handler(context, inputs):
     :return The desired machine name.
     """
     old_name = inputs["resourceNames"][0]
-    new_name = inputs["customProperties"]["Name12345"]
+    new_name = inputs["customProperties"]["Name"]
 
     outputs = {}
     outputs["resourceNames"] = inputs["resourceNames"]
